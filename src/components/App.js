7677a716e0a7b2513564a5d709b3dc5eb1cd4e54
lunaseifert <thepuddingdeity@gmail.com>
2022-12-03 15:39:54 -0600

    commit tree c873ebc000bbb226b828d43a14475792d43aca40

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState(null)
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/questions')
+    .then(res => res.json())
+    .then(data => setQuestions(data))
+  }, [])
+  
+  function onPost(newQuestion){
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDelete(id){
+    setQuestions(questions.filter(question => question.id !== id))
+  }
+
+  function handlePatch(item){
+    setQuestions(questions.map(question =>{
+      if (question.id === item.id){
+        return item
+      }
+      else{
+        return question
+      }
+    }))
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onPost={onPost}/> : <QuestionList questions={questions} onDelete={handleDelete} onPatch={handlePatch}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React from "react"
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+function QuestionList({questions, onDelete, onPatch}) {
+  if (!questions){
+    return <p>Loading</p>
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question =>{
+        return <QuestionItem key={question.id} question={question} onDelete={onDelete} onPatch={onPatch}/>
+      })}</ul>
     </section>
   );
 }
